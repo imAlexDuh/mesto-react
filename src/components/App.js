eeebee0a8e7855fc3fd4de6250de6031a122a2fc
imAlexDuh <4uva40k99@gmail.com>
2023-08-28 18:47:40 +0300

    Sprint 10, Third try

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
     const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
     const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
     const [isImgPopupOpen, setIsImgPopupOpen] = React.useState(false);
-    const [selectedCard, setSelectedCard] = React.useState([]);
+    const [selectedCard, setSelectedCard] = React.useState({});
 
     function handleEditProfileClick() {
         setEditProfilePopupOpen(true);
